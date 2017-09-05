cf08c82630e1d602fa19d8f00e67e2f56982dff3
Natalie Perpepaj <perpepajn25@gmail.com>
2017-09-05 17:09:30 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ActiveRecord::Base
+
+  validates :title, presence: true
+  validates :content, length: { minimum: 250}
+  validates :summary, length: { maximum: 250}
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+  validate :non_clickbait
+
+  def non_clickbait
+    if self.title
+      if !self.title.match /Won't Believe|Secret|Top [number]|Guess/
+        self.errors.add(:title, "can't be non-clicbait")
+      end
+    end
+  end
+
 end
